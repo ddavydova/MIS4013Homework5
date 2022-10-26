64e52190b30e8ed73bbe5f65dbb46b0f57e00351
Dasha Davydova <dasha.davydova@ou.edu>
2022-10-26 14:07:42 -0500

    Update f1.php

--- f1.php
+++ f1.php
@@ -1,5 +1,10 @@
 <?php include("header.php");?>
 <?php include("links.php");?>
 
+<script>
+        let d = new Date();
+        alert("Today's date is " + d);
+    </script>
+
 <?php include("footer.php");?>
 
