3f8fb01338625e3126306d23e273f894063582d7
Dasha Davydova <dasha.davydova@ou.edu>
2022-10-26 14:28:32 -0500

    Update f4.php

--- f4.php
+++ f4.php
@@ -1,26 +1,105 @@
 <?php include("header.php");?>
 <?php include("links.php");?>
  
-<body>   
-    <center>  
-    <h2> Example: Beep a sound </h2>  
-    <h4> Press the Button to beep a sound </h4>   
+<head>  
+<script>  
   
-    <!-- Use audio tab to provide an id to sound for further using it in JavaScript code -->  
-    <audio id="beepAudio" >   
-    <!-- Provide a link of beep sound from internet -->  
-    <source src= "https://www.soundjay.com/buttons/sounds/button-4.mp3">   
-    </audio>   
-    <button onclick="beepSound()"> Click to beep </button>   
-  
-    <script>   
-           //user-defined function to play a beep sound  
-    var audio = document.getElementById(' beepAudio');   
-    function beepSound () {   
-        audio.play()   
+function ageCalculator() {  
+    //collect input from HTML form and convert into date format  
+    var userinput = document.getElementById("DOB").value;  
+    var dob = new Date(userinput);  
+      
+    //check user provide input or not  
+    if(userinput==null || userinput==''){  
+      document.getElementById("message").innerHTML = "**Choose a date please!";    
+      return false;   
     }   
-    </script>   
-    </center>  
-</body>   
+      
+    //execute if the user entered a date   
+    else {  
+    //extract the year, month, and date from user date input  
+    var dobdobYear = dob.getYear();  
+    var dobdobMonth = dob.getMonth();  
+    var dobdobDate = dob.getDate();  
+      
+    //get the current date from the system  
+    var now = new Date();  
+    //extract the year, month, and date from current date  
+    var currentYear = now.getYear();  
+    var currentMonth = now.getMonth();  
+    var currentDate = now.getDate();  
+      
+    //declare a variable to collect the age in year, month, and days  
+    var age = {};  
+    var ageString = "";  
+    
+    //get years  
+    yearAge = currentYear - dobYear;  
+      
+    //get months  
+    if (currentMonth >= dobMonth)  
+      //get months when current month is greater  
+      var monthAge = currentMonth - dobMonth;  
+    else {  
+      yearAge--;  
+      var monthAge = 12 + currentMonth - dobMonth;  
+    }  
+  
+    //get days  
+    if (currentDate >= dobDate)  
+      //get days when the current date is greater  
+      var dateAge = currentDate - dobDate;  
+    else {  
+      monthAge--;  
+      var dateAge = 31 + currentDate - dobDate;  
+  
+      if (monthAge < 0) {  
+        monthAge = 11;  
+        yearAge--;  
+      }  
+    }  
+    //group the age in a single variable  
+    age = {  
+    years: yearAge,  
+    months: monthAge,  
+    days: dateAge  
+    };  
+        
+        
+    if ( (age.years > 0) && (age.months > 0) && (age.days > 0) )  
+       ageageString = age.years + " years, " + age.months + " months, and " + age.days + " days old.";  
+    else if ( (age.years == 0) && (age.months == 0) && (age.days > 0) )  
+       ageString = "Only " + age.days + " days old!";  
+    //when current month and date is same as birth date and month  
+    else if ( (age.years > 0) && (age.months == 0) && (age.days == 0) )  
+       ageageString = age.years +  " years old. Happy Birthday!!";  
+    else if ( (age.years > 0) && (age.months > 0) && (age.days == 0) )  
+       ageageString = age.years + " years and " + age.months + " months old.";  
+    else if ( (age.years == 0) && (age.months > 0) && (age.days > 0) )  
+       ageageString = age.months + " months and " + age.days + " days old.";  
+    else if ( (age.years > 0) && (age.months == 0) && (age.days > 0) )  
+       ageageString = age.years + " years, and" + age.days + " days old.";  
+    else if ( (age.years == 0) && (age.months > 0) && (age.days == 0) )  
+       ageageString = age.months + " months old.";  
+    //when current date is same as dob(date of birth)  
+    else ageString = "Welcome to Earth! <br> It's first day on Earth!";   
+  
+    //display the calculated age  
+    return document.getElementById("result").innerHTML = ageString;   
+               
+  }  
+}  
+</script>  
+</head>  
+<body>  
+<center>  
+<h2 style="color: 32A80F" align="center"> Calculate Age from Date of Birth <br> <br> </h2>   
+  
+<b> Enter Date of Birth: <input type=date id = DOB>  </b>  
+<span id = "message" style="color:red"> </span> <br><br>    
+<button type="submit" onclick = "ageCalculator()"> Calculate age </button> <br><br>  
+<h3 style="color:32A80F" id="result" align="center"></h3>   
+</center>  
+</body> 
 
 <?php include("footer.php");?>
