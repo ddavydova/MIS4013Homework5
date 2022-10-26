616d70dd7c12e433714b90cde48d77bf0bc9eedb
Dasha Davydova <dasha.davydova@ou.edu>
2022-10-26 14:10:42 -0500

    Update f2.php

--- f2.php
+++ f2.php
@@ -1,3 +1,44 @@
 <?php include("header.php");?>
 <?php include("links.php");?>
+
+<style>  
+#myContainer {  
+  width: 350px;  
+  height: 350px;  
+  position: relative;  
+  background: green;  
+}  
+#myAnimation {  
+  width: 45px;  
+  height: 45px;  
+  position: absolute;  
+  background-color: rgb(226, 43, 43);  
+}  
+</style>  
+
+<p>  
+<button onclick="myMove()">Click Here</button>   
+</p>  
+<div id ="myContainer">  
+<div id ="myAnimation"></div>  
+</div>  
+<script>  
+var id = null;  
+function myMove() {  
+  var elem = document.getElementById("myAnimation");     
+  var pos = 0;  
+  clearInterval(id);  
+  id = setInterval(frame, 10);  
+  function frame() {  
+    if (pos == 300) {  
+      clearInterval(id);  
+    } else {  
+      pos++;   
+      elem.style.top = pos + 'px';   
+      elem.style.left = pos + 'px';   
+    }  
+  }  
+}  
+</script>    
+
 <?php include("footer.php");?>
